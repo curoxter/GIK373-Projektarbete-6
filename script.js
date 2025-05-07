6feedb20549a468c3dcd1fbf6eb0a92971c05ffc
curoxter <h23izaso@du.se>
2025-05-07 15:05:12 +0200

    new data added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+/* const fs = require('fs');
 const csv = require('csv-parser');
 
 fs.createReadStream('./data.csv')
@@ -10,4 +10,16 @@ fs.createReadStream('./data.csv')
   })
   .on('end', () => {
     console.log('Only Swedish data processed.');
-  });
+  }); */
+
+  const fs = require('fs');
+  const csv = require('csv-parser');
+  
+  fs.createReadStream('data_animal.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      console.log(row);
+    })
+    .on('end', () => {
+      console.log('CSV file successfully processed.');
+    });
